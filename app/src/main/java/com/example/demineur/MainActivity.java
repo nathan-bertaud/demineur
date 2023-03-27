f3127f16ca4eec8a14feecb9ee3b595f4bea43df
Romain0424 <60342445+Romain0424@users.noreply.github.com>
2023-03-27 15:08:28 +0200

    First commit + début service
    First commit + début service

--- app/src/main/java/com/example/demineur/MainActivity.java
+++ app/src/main/java/com/example/demineur/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.demineur;
+import android.content.BroadcastReceiver;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 
 import com.example.demineur.databinding.ActivityMainBinding;
 
@@ -10,10 +13,53 @@ public class MainActivity extends AppCompatActivity {
 
 
     private ActivityMainBinding binding;
+    private Intent intentService;
+    static public final String BROADCAST = "timer.projet";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        intentService = new Intent(this,MyService.class);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startService(intentService);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver,new IntentFilter(BROADCAST));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopService(intentService);
+    }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                int number = bundle.getInt("question");
+                boolean value = bundle.getBoolean("result");
+                update(number,value);
+            }
+        }
+    };
+
+
+
 }
